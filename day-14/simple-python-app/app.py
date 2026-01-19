5434c4555a0d441daca660f5fa98ffb32f8d571b
Usama Aijaz <65039841+UsamaTanoli@users.noreply.github.com>
2026-01-19 07:38:37 +0500

    Update greeting message in Flask app

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, from Usama Tanoli!'
+    return 'Hello, from Usama Tanoli & Gemini'
 
 if __name__ == '__main__':
     app.run()
